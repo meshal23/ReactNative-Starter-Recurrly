53e34ea5c11a90852b4358c14b7d026bcf601175
Meshal <meshal@example.com>
2026-08-20 07:37:44 +0530

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text } from "react-native";
 import { styled } from "nativewind";
+import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView);
@@ -9,38 +9,25 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 p-5 bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-5xl font-sans-extrabold">Home</Text>
+
       <Link
         href="/onboarding"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded font-sans-bold  bg-primary text-white p-4"
       >
         Go Onboarding
       </Link>
       <Link
         href="/(auth)/sign-in"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded font-sans-bold bg-primary text-white p-4"
       >
         Go To SignIn
       </Link>
       <Link
         href="/(auth)/sign-up"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded font-sans-bold bg-primary text-white p-4"
       >
         Go To SignUp
-      </Link>
-      <Link
-        href="/subscriptions/spotify"
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Spotify Subscription
-      </Link>
-      <Link
-        href={{ pathname: "/subscriptions/[id]", params: { id: "claude" } }}
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Claude Max Subscription
       </Link>
     </SafeAreaView>
   );
