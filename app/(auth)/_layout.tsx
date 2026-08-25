e2392f2488209169c8fcfb0fc7d8f15afa942702
Meshal <meshal@example.com>
2026-08-25 14:15:48 +0530

    feat:clerk authentication implementation

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,25 @@
 import "@/global.css";
-import { Stack } from "expo-router";
+import { Redirect, Slot, Stack } from 'expo-router';
+import { useAuth } from '@clerk/expo';
+import { ActivityIndicator, View } from 'react-native';
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: true }} />;
+  const { isSignedIn, isLoaded } = useAuth();
+
+  // Prevent flash of unauthenticated content while loading tokenCache
+  if (!isLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  // Redirect to your app's home or main screen if already signed in
+  if (isSignedIn) {
+    return <Redirect href="/" />;
+  }
+
+
+  return <Slot screenOptions={{ headerShown: false }} />;
 }
